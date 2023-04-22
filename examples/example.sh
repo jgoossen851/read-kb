7d87981b8d310e33a56690d621a12845b1c002d7
Jeremy Goossen <jeremyg995@gmail.com>
2023-04-22 21:49:50 +0000

    Develop example for only one daemon at a time

--- examples/example.sh
+++ examples/example.sh
@@ -4,12 +4,12 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 TEMP_DIR="/tmp/read-kb"
 BASH_DAEMON="daemon.sh"
 CPP_DAEMON="../build/examples/read-kb-example-daemon"
+DAEMON="${BASH_DAEMON}"
 
 
 cleanup() {
   # Send command through pipe to shutdown
-  echo 'SIGINT' > "${TEMP_DIR}/ipipe_bash"
-  echo 'SIGINT' > "${TEMP_DIR}/ipipe_cpp"
+  echo 'SIGINT' > "${TEMP_DIR}/ipipe"
 
   # Delete all temp files
   rm -rf "${TEMP_DIR}"
@@ -24,32 +24,21 @@ echo "Press 'Ctrl-C' to quit."
 
 # Create a pipe for input to the daemon
 mkdir -p "${TEMP_DIR}"
-if [ ! -e "${TEMP_DIR}/ipipe_bash" ]; then
-  mkfifo "${TEMP_DIR}/ipipe_bash"
-fi
-if [ ! -e "${TEMP_DIR}/ipipe_cpp" ]; then
-  mkfifo "${TEMP_DIR}/ipipe_cpp"
+if [ ! -e "${TEMP_DIR}/ipipe" ]; then
+  mkfifo "${TEMP_DIR}/ipipe"
 fi
 
 
 # Start the daemon in the background with input pipe to read
-if [ ! -f "${SCRIPT_DIR}/${BASH_DAEMON}" ]; then
-  echo "${SCRIPT_DIR}/${BASH_DAEMON}" could not be found
+if [ ! -f "${SCRIPT_DIR}/${DAEMON}" ]; then
+  echo "${SCRIPT_DIR}/${DAEMON}" could not be found
   exit
 fi
-"${SCRIPT_DIR}/${BASH_DAEMON}" "${TEMP_DIR}/ipipe_bash" &
-
-if [ ! -f "${SCRIPT_DIR}/${CPP_DAEMON}" ]; then
-  echo "${SCRIPT_DIR}/${CPP_DAEMON}" could not be found
-  exit
-fi
-"${SCRIPT_DIR}/${CPP_DAEMON}"  "${TEMP_DIR}/ipipe_cpp" &
-# "${SCRIPT_DIR}/${CPP_DAEMON}"  "text.txt" &
+"${SCRIPT_DIR}/${DAEMON}" "${TEMP_DIR}/ipipe" &
 
 
 while true; do
   # Send keyboard events to the pipe
   KEY="$(read-kb 2>/dev/null)"
-  echo "${KEY}" > "${TEMP_DIR}/ipipe_bash"
-  echo "${KEY}" > "${TEMP_DIR}/ipipe_cpp"
+  echo "${KEY}" > "${TEMP_DIR}/ipipe"
 done
