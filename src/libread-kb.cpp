9d52cab73c2b6a19eba90a27f4886cc31a33e4ca
Jeremy Goossen <jeremyg995@gmail.com>
2023-05-06 21:00:09 +0000

    Stop accessing pfds as an array (access as pointer)

--- src/libread-kb.cpp
+++ src/libread-kb.cpp
@@ -134,17 +134,17 @@ ReadKB::ReadKB() {
   term.c_lflag &= ~ECHO; // Do not print input
   tcsetattr(STDIN_FD, TCSANOW, &term);
 
-  // Get file descriptors for the poll command
+  // Allocate memory for file descriptor for the poll command
   pfds = static_cast<pollfd*>(calloc(1, sizeof(struct pollfd)));
   errorIf(pfds == NULL, "malloc");
 
-  // Open each file, and add it to 'pfds' array
-  pfds[0].fd = STDIN_FD;
-  errorIf(pfds[0].fd == -1, "open");
-  printlog("Opened \"%s\" on fd %d\n", "stdin", pfds[0].fd);
+  // Assign file descriptor to poll structure
+  pfds->fd = STDIN_FD;
+  errorIf(pfds->fd == -1, "open");
+  printlog("Opened \"%s\" on fd %d\n", "stdin", pfds->fd);
 
   // Request poll() to scan file descriptor for data available to read (POLLIN signal)
-  pfds[0].events = POLLIN;
+  pfds->events = POLLIN;
 }
 
 ReadKB::Key ReadKB::read_key() const {
@@ -161,19 +161,19 @@ ReadKB::Key ReadKB::read_key() const {
     // Read from pipe one buffer-length at a time
     u_char buf[BUFF_SIZE_CHARS];
 
-    if (pfds[0].revents != 0) {
+    if (pfds->revents != 0) {
 
       // Log signals (events) found by poll
-      printlog("  fd=%d; events: %s%s%s%s\n", pfds[0].fd,
-          (pfds[0].revents & POLLIN)   ? "\033[32mPOLLIN\033[0m "   : "",
-          (pfds[0].revents & POLLHUP)  ? "\033[33mPOLLHUP\033[0m "  : "",
-          (pfds[0].revents & POLLERR)  ? "\033[31mPOLLERR\033[0m "  : "",
-          (pfds[0].revents & POLLNVAL) ? "\033[31mPOLLNVAL\033[0m " : "");
+      printlog("  fd=%d; events: %s%s%s%s\n", pfds->fd,
+          (pfds->revents & POLLIN)   ? "\033[32mPOLLIN\033[0m "   : "",
+          (pfds->revents & POLLHUP)  ? "\033[33mPOLLHUP\033[0m "  : "",
+          (pfds->revents & POLLERR)  ? "\033[31mPOLLERR\033[0m "  : "",
+          (pfds->revents & POLLNVAL) ? "\033[31mPOLLNVAL\033[0m " : "");
 
-      if (pfds[0].revents & POLLIN) {
+      if (pfds->revents & POLLIN) {
         // Read from the pipe if there is data available (POLLIN)
 
-        ssize_t s = read(pfds[0].fd, buf, sizeof(buf));
+        ssize_t s = read(pfds->fd, buf, sizeof(buf));
         errorIf(s == -1, "read");
         errorIf(s + 1 > BUFF_SIZE_CHARS, "read buffer overflow");
         printlog("    read %zd bytes: \033[1m", s);
